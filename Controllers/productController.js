c39c970acfe4666476470a2511634e2dc43420c5
Mahmoudwa2l <mahmoud.wael5@msa.edu.eg>
2023-10-24 01:02:01 +0300

    new

--- Controllers/productController.js
+++ Controllers/productController.js
@@ -52,23 +52,8 @@ const getProduct = async (req, res) => {
 
 //GET ALL PRODUCTS
 const getAllProducts = async (req, res) => {
-  const qNew = req.query.new;
-  const qCategory = req.query.category;
   try {
-    let products;
-
-    if (qNew) {
-      products = await Product.find().sort({ createdAt: -1 }).limit(1);
-    } else if (qCategory) {
-      products = await Product.find({
-        categories: {
-          $in: [qCategory],
-        },
-      });
-    } else {
-      products = await Product.find();
-    }
-
+    products = await Product.find();
     res.status(200).json(products);
   } catch (err) {
     res.status(500).json(err);
